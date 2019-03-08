12a3dbdf3d3fa96400d9acdd8278b8d25ba021dc
bijay013 <48025193+bijay013@users.noreply.github.com>
2019-03-08 21:00:08 +0545

    1.2

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from bullet import Bullet
+from alien import Alien
+
 def check_events(ai_settings, screen, ship, bullets):
 	"""Responds to keypresses and mouse events."""
 	for event in pygame.event.get():
@@ -13,7 +15,7 @@ def check_events(ai_settings, screen, ship, bullets):
 			 check_keyup_events(event, ship)
 			
 				
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
 	""" Update images on the screen and flip to the new screen."""
 	# Redraw the screen during each pass through the loop.
 	screen.fill(ai_settings.bg_color)
@@ -21,6 +23,7 @@ def update_screen(ai_settings, screen, ship, bullets):
 	for bullet in bullets.sprites():
 		bullet.draw_bullet()
 	ship.blitme()
+	aliens.draw(screen)
 		
 	# Make the most recently drawn screen visible.
 	pygame.display.flip()
@@ -33,7 +36,8 @@ def check_keydown_events(event,ai_settings, screen, ship, bullets):
 		ship.moving_left = True
 	if event.key == pygame.K_SPACE:
 		fire_bullet(ai_settings, screen, ship, bullets)
-		
+	if event.key == pygame.K_q:
+		sys.exit()
 		
 def check_keyup_events(event, ship):
 	"""Respond to key releases."""
@@ -58,5 +62,22 @@ def fire_bullet(ai_settings, screen, ship, bullets):
 	if len(bullets) < ai_settings.bullets_allowed:
 		new_bullet = Bullet(ai_settings, screen, ship)
 		bullets.add(new_bullet)
+
+def create_fleet(ai_settings, screen, aliens):
+	"""Create a full fleet of aliens."""
+	# Create an alien and find the number of aliens in a row.
+	# Spacing between each alien equal to one width.
+	alien = Alien(ai_settings, screen)
+	alien_width = alien.rect.width
+	available_space_x = ai_settings.screen_width - 2 * alien_width
+	number_aliens_x = int(available_space_x / (2 * alien_width))
+	
+	# Create the first row of aliens.
+	for alien_number in range(number_aliens_x):
+		# Create an alien and place it in the row.
+		alien = Alien(ai_settings, screen)
+		alien.x = alien_width + 2 * alien_width * alien_number
+		alien.rect.x = alien.x
+		aliens.add(alien)
 	
 	
